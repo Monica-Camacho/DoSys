354358bad3e2717436c578d311c1c7f333680093
MoniCamacho <119087190+Monica-Camacho@users.noreply.github.com>
2025-07-23 17:25:51 -0600

    editar head listo

--- organizacion_usuarios.php
+++ organizacion_usuarios.php
@@ -86,11 +86,21 @@ $conexion->close();
     <meta charset="utf-8">
     <title>DoSys - Gestionar Voluntarios</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    
+    <!-- Favicon -->
     <link rel="icon" type="image/png" href="img/logos/DoSys_chico.png">
-    <link href="https://fonts.googleapis.com/css2?family=DM+Sans&family=Inter&display=swap" rel="stylesheet">
+
+    <!-- Google Web Fonts, Iconos y Estilos -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Inter:slnt,wght@-10..0,100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+    
+    <!-- Estilos de Bootstrap y de la Plantilla -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
+    
 </head>
 <body>
     <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center"><div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status"></div></div>
