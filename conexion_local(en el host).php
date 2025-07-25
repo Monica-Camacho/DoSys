aa2e01cc2259dc9f949c693c0a030401530e04fe
MoniCamacho <119087190+Monica-Camacho@users.noreply.github.com>
2025-07-25 10:41:24 -0600

    archivos host

---
+++ conexion_local(en el host).php
@@ -0,0 +1,13 @@
+<?php
+$servidor = "191.96.56.1"; // Cambiar si se usa un servidor remoto
+$usuario = "u312858745_root"; // Usuario de la base de datos
+$clave = "1dosys_Qwert?"; // Contraseña de la base de datos
+$base_datos = "u312858745_dosys"; // Nombre de tu base de datos
+
+$conexion = new mysqli($servidor, $usuario, $clave, $base_datos);
+
+if ($conexion->connect_error) {
+    die("Conexión fallida: " . $conexion->connect_error);
+}
+?>
+
---
+++ config(en el host).php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Archivo de Configuración Principal de DoSys
+ */
+
+// ==================================================================
+// PARA DEPURACIÓN: Muestra errores detallados.
+// ¡Recuerda quitar estas 3 líneas antes de pasar a producción!
+// ==================================================================
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+//error_reporting(E_ALL);
+
+
+// ==================================================================
+// URL BASE DEL PROYECTO
+// Define la "dirección completa" de tu sitio para que las rutas
+// a imágenes, CSS y otros enlaces siempre funcionen.
+//
+// -> Si tu proyecto se accede desde http://localhost/
+//    Usa: define('BASE_URL', '/');
+//
+// -> Si tu proyecto se accede desde http://localhost/dosys/
+// ==================================================================
+//define('BASE_URL', 'http://localhost/Dosys/');
+
+// -> Si tu proyecto se accede desde http://localhost/dosys/
+define('BASE_URL', 'https://dosys.mx/');
+
+/*
+|--------------------------------------------------------------------------
+| CONFIGURACIÓN DE SERVIDOR DE CORREO (SMTP)
+|--------------------------------------------------------------------------
+|
+| Credenciales para el envío de correos a través de PHPMailer.
+| Es una buena práctica mantenerlas aquí en lugar de directamente en el script.
+|
+*/
+define('SMTP_HOST', 'smtp.hostinger.com');
+define('SMTP_USER', 'contacto@dosys.mx');
+define('SMTP_PASS', 'wg=WO24L49_R'); // ¡Considera usar variables de entorno para esto en producción!
+define('SMTP_PORT', 465);
+
+?>
